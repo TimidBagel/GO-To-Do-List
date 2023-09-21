0e7f98cf318c93fccb3427fde4695fc82eda60f5
TimidBagel <slaprabbit805@gmail.com>
2023-09-20 21:46:01 -0400

    added some stuff

--- cli_tdl/main.go
+++ cli_tdl/main.go
@@ -25,6 +25,7 @@ func HelpHelp() {
 }
 
 func TaskHelp(args ...string) error {
+	// checks if correct number of arguments are passed
 	if len(args) != 2 {
 		return errors.New("unexpected number of arguments")
 	}
@@ -34,74 +35,118 @@ func TaskHelp(args ...string) error {
 	fmt.Printf("--> %50s - %-30s\n", "'task del [name]'", "deletes given task")
 	fmt.Printf("--> %50s - %-30s\n", "'task done [name]'", "marks a given task as completed")
 
+	// return nil error if success
 	return nil
 }
 
 func QuitHelp(args ...string) error {
+	// checks if correct number of arguments are passed
 	if len(args) != 2 {
 		return errors.New("unexpected number of arguments")
 	}
 
 	fmt.Println("- 'quit 0' - ends program with 0 exit code")
+
+	// return nil error if success
 	return nil
 }
 
+// Takes a list of strings as a parameter, will return an error if there are more or less than 4 strings in that list.
+// Iniitializes a new task, creates a new task file, and writes the serialized task JSON to that file. Returns a nil error
+// if successful.
 func NewTask(args ...string) error {
+	// checks if correct number of arguments are passed
 	if len(args) != 4 {
 		return errors.New("unexpected number of arguments")
 	}
 
+	// initializes name and description
 	name := args[2]
 	desc := args[3]
 
+	// initialize path based on name
 	path := "tasks/" + name + ".json"
 
+	// check if path exists
 	_, err := os.Stat(path)
 
+	// if does exist, return error. if doesn't exist and error, return error.
 	if os.IsExist(err) {
 		return errors.New("task name already exists")
 	} else if !os.IsNotExist(err) {
 		return err
 	}
 
+	// create file at path
 	file, err := os.Create(path)
 
+	// return error if failed
 	if err != nil {
 		return err
 	}
 
+	// closes file after completion
+	defer file.Close()
+
+	// initialize new task
 	task := Task{name, desc, false}
 
+	// convert task to JSON, write JSON to task file
 	err = Serialize(task, file)
 
+	// return error if failed
 	if err != nil {
 		return err
 	}
 
+	// return nil error if success
 	return nil
 }
 
 func EditTask(args ...string) error {
+	// checks if correct number of arguments are passed
 	if len(args) != 5 {
 		return errors.New("unexpected number of arguments")
 	}
 
+	path := "tasks/" + args[2] + ".json"
+
+	_, err := os.Stat(path)
+
+	if os.IsNotExist(err) {
+		return errors.New("task not found")
+	} else if !os.IsExist(err) {
+		return err
+	}
+
+	name := args[3]
+	desc := args[4]
+
+	task := Task{name, desc, false}
+
+	fmt.Print(task)
+
+	// return nil error if success
 	return nil
 }
 
 func DeleteTask(args ...string) error {
+	// checks if correct number of arguments are passed
 	if len(args) != 3 {
 		return errors.New("unexpected number of arguments")
 	}
 
+	// return nil error if success
 	return nil
 }
 
 func CompleteTask(args ...string) error {
+	// checks if correct number of arguments are passed
 	if len(args) != 3 {
 		return errors.New("unexpected number of arguments")
 	}
 
+	// return nil error if success
 	return nil
 }
 
@@ -147,7 +192,6 @@ func main() {
 						continue
 					}
 				} else {
-					fmt.Printf("- Error: %v\n- *see 'help'* -\n", errors.New("unrecognized command string"))
 				}
 
 			} else {
@@ -159,6 +203,9 @@ func main() {
 	}
 }
 
+// Serializes a Task struct into JSON, then writes that JSON byte array to a file. Takes
+// two parameters, the Task struct, and type *os.File.
+// Returns an error message or nil.
 func Serialize(task Task, file *os.File) error {
 	// converts task to JSON bytes
 	jsonData, err := json.MarshalIndent(task, "", "   ")
@@ -167,6 +214,9 @@ func Serialize(task Task, file *os.File) error {
 		return err
 	}
 
+	// closes file
+	defer file.Close()
+
 	// writes converted JSON to file
 	_, err = file.Write(jsonData)
 
@@ -174,41 +224,39 @@ func Serialize(task Task, file *os.File) error {
 		return err
 	}
 
-	// closes file
-	file.Close()
-
 	return nil
 }
 
-func Deserialize(path string) ([]Task, error) {
+// Deserializes a given task file. Takes one parameter: the path of the file. Returns
+// a Task struct and an error, possibly nil.
+func Deserialize(path string) (Task, error) {
 
-	var tasks []Task
+	var task Task
 
 	// reads file as bytes
 	jsonData, err := os.ReadFile(path)
 
 	// handles error
 	if err != nil {
-		fmt.Printf("Error reading file '%s': %v\n", path, err)
-		return tasks, err
+		return task, err
 	}
 
+	// checks if JSON is valid
 	valid := json.Valid(jsonData)
 
 	if !valid {
 		fmt.Println("yo json aint valid cheif!!1!")
-		return tasks, errors.New("invlaid JSON")
+		return task, errors.New("invlaid JSON")
 	}
 
 	// deserializes bytes, stored in task list
-	err = json.Unmarshal(jsonData, &tasks)
+	err = json.Unmarshal(jsonData, &task)
 
 	// handles error
 	if err != nil {
-		fmt.Println("Error deserializing file:", err)
-		return tasks, err
+		return task, err
 	}
 
 	// returns deserialized task list
-	return tasks, nil
+	return task, nil
 }
